e7ef370c0cc75d781b0e55c9ff54a4590cf11a76
dvpermyakov <dvpermyakov1@gmail.com>
2015-04-11 13:58:53 +0300

    unneeded file

---
+++ pizza_delivery_django/pizza_delivery_app/migrations/0006_remove_address_new_filed.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pizza_delivery_app', '0005_address_new_filed'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='address',
+            name='new_filed',
+        ),
+    ]
