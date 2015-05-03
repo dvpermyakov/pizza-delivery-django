31c3f48cee1cd2610e7c26a92cdcd499960c7552
dvpermyakov <dvpermyakov1@gmail.com>
2015-05-03 23:49:42 +0300

    minor changes

--- pizza_delivery_django/pizza_delivery_app/views/api/user/rating.py
+++ pizza_delivery_django/pizza_delivery_app/views/api/user/rating.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from django.http import HttpResponseBadRequest, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from pizza_delivery_app.models import User, Rating, Product
@@ -8,7 +9,7 @@ __author__ = 'dvpermyakov'
 
 @csrf_exempt
 def set_rating(request):
-    user_id = request.GET.get('user_id')
+    user_id = request.POST.get('user_id')
     try:
         user = User.objects.get(id=user_id)
     except User.DoesNotExist:
@@ -22,10 +23,13 @@ def set_rating(request):
             product = Product.objects.get(id=rating.get('product_id'))
         except Product.DoesNotExist:
             return HttpResponseBadRequest()
-        rating_obj = Rating.objects.get(user=user, product=product)
-        if rating_obj.rating != float(rating.get('rating')):
-            rating_obj.rating = float(rating.get('rating'))
-            rating_obj.save()
+        try:
+            rating_obj = Rating.objects.get(user=user, product=product)
+            if rating_obj.rating != float(rating.get('rating')):
+                rating_obj.rating = float(rating.get('rating'))
+                rating_obj.save()
+        except Rating.DoesNotExist:
+            Rating(user=user, product=product, rating=float(rating.get('rating'))).save()
     return JsonResponse({
         'success': True
     })
